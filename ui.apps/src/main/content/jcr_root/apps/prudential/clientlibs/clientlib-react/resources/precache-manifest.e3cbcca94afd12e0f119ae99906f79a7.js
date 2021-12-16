f262761d5bbccb5665bf1602d2b242efdc329e8e
Andressa Schinoff <andressa.schinoff@valtech.com>
2021-12-16 17:18:12 -0300

    react integration

---
+++ ui.apps/src/main/content/jcr_root/apps/prudential/clientlibs/clientlib-react/resources/precache-manifest.e3cbcca94afd12e0f119ae99906f79a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cfcc13fce5ee1c95123ccf8c20a492",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "ddc78814c56876982502",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/static/css/main.2a17b812.chunk.css"
+  },
+  {
+    "revision": "5a13c0ebeaf7a9f4a4c5",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/static/js/2.f19d20bb.chunk.js"
+  },
+  {
+    "revision": "e37d8a22560034f76a82da2144060987",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/static/js/2.f19d20bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc78814c56876982502",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/static/js/main.b0c59867.chunk.js"
+  },
+  {
+    "revision": "3ca1b5a1d573a5d92867",
+    "url": "/etc.clientlibs/prudential/clientlibs/clientlib-react/resources/static/js/runtime-main.0cc2fe32.js"
+  }
+]);
